8fecef27e1b652a8ce31c2cdb103fe3640560102
degnbol <christian.degnbol@gmail.com>
2015-09-26 16:32:44 +0200

    read funktion
    indlæsning af det jeg har lavet inden github. read funktion og main samt
    data.

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 13:33:39 2015
+
+@author: Christian
+"""
+
+import os
+from readHumanData import *
+
+os.chdir('/Users/Christian/OneDrive/Fagprojekt/python')
+
+sequence, meas = readHumanData('data.txt')
+
+
+
+
---
+++ readHumanData.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 15:41:26 2015
+
+@author: Christian
+"""
+
+def readHumanData(path):
+    
+    sequence = []
+    meas = []
+
+    with open(path) as data:
+        for line in data:
+            l = line.split()
+            if (l[0] == 'human'
+            and l[1] == 'HLA-A-0201'
+            and l[2] == '9'):
+                sequence.append(l[4])
+                meas.append(l[6])
+    
+    return sequence, meas
