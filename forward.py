b4d2a0a1a8aa99d10a28a17e7883ff4ea3e6c89e
degnbol <christian.degnbol@gmail.com>
2015-09-28 19:24:25 +0200

    tilføjed forward funktion
    omstrukturering og tilføjelse af forward

---
+++ forward.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 18:57:30 2015
+
+@author: Christian
+"""
+
+import numpy as np
+from activation import activation
+
+def forward(inputLayer, weightMatrix1, weightMatrix2):
+            
+    # beregn foreløbig hidden layer vha. w matrix og binær sekvens
+    hiddenLayer = np.dot(weightMatrix1, inputLayer)
+    
+    # brug blød threshold på hidden layer: y = 1/(1 + exp(-x))
+    hiddenLayer = activation(hiddenLayer)
+    
+    # beregn resultat med hidden layer og w matrix 2 (som blot er liggende vektor)
+    outputLayer = np.dot(weightMatrix2, hiddenLayer)
+    
+    # brug blød threshold på resultat
+    outputLayer = activation(outputLayer)    
+    
+    return outputLayer
--- main.py
+++ main.py
@@ -5,32 +5,39 @@ Created on Sat Sep 26 13:33:39 2015
 @author: Christian
 """
 
+# importer ting
 import os
 import sys
 import matplotlib.pyplot as plot
 
+# ændr sti så vi kan finde hjælpefunktioner
 projectPath = os.path.dirname(os.path.abspath(sys.argv[0]))
 os.chdir(projectPath)
 
+# importer hjælpefunktioner
 from readHLA import readHLA
 from logTransform import logTransform
 from peptideToBinary import peptideToBinary
+from forward import forward
 
+# indlæs sekvenser og hvor godt de binder til mhc proteinet
 sequence, meas = readHLA('data.txt')
+
+# log transformer measurements så de er pænere tal
 meas = logTransform(meas)
 
+# bare for sjov, så man kan se fordelingen af binding affinities
 plot.hist(meas)
 
+# lav en tilfældig sekvens om til binær
 inputLayer = peptideToBinary(sequence[0])
 
+# vælg antal nodes i hidden layer
+numOfHiddenNodes = 8
 
 # lav vægt matrix med tilfældige værdier
+weightMatrix1 = [] # = randomMatrix(len(inputlayer), numOfHiddenNodes)
+weightMatrix2 = [] # = randomMatrix(numOfHiddenNodes, 1)
 
-# beregn foreløbig hidden layer vha. w matrix og binær sekvens
-
-# brug blød threshold på hidden layer: y = 1/(1 + exp(-x))
-
-# beregn resultat med hidden layer og w matrix 2 (som blot er liggende vektor)
-
-# brug blød threshold på resultat
-
+# kør forward funktion med tilfældige vægt matricer
+outputLayer = forward(inputLayer, weightMatrix1, weightMatrix2)
